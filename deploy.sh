06eec1563ccfe504950556338119c0bfb6e394cc
Николай Кот <isemenov-2018@yandex.ru>
2020-06-15 18:37:34 +0300

    deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#cd /home/nvkot.ru/www/nvkot/
+cd /home/openschool/www
 git pull origin master
 #docker restart nvkot_redactor  nvkot_author nvkot_admin  nvkot_reader
 #docker exec -it nvkot_admin python3 manage.py migrate
